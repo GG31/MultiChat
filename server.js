c3b3dcf224d6a702df3326df42de2e2572214d81
Genevieve Cirera <genevieve.cirera@gmail.com>
2015-01-08 12:45:36 +0100

    connection to mongoDB + clean node_modules

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ var app = http.createServer(function (req, res) {
   file.serve(req, res);
 }).listen(2013);
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
 // var express = require('express');
 // var app = express();
 // console.log(express.static(__dirname + '/js'));
