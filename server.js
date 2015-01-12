f732ca6c00db25b9f710c1eb27d68be22b8f84d1
Genevieve Cirera <cirera@polytech.unice.fr>
2015-01-12 13:47:41 +0100

    merge

--- server.js
+++ server.js
@@ -5,18 +5,7 @@ var app = http.createServer(function (req, res) {
   file.serve(req, res);
 }).listen(2013);
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
-});
+var mongo = require('./mongo.js');
 // var express = require('express');
 // var app = express();
 // console.log(express.static(__dirname + '/js'));
