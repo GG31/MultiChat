90384d29b2d2f2000f9245712326144df6bc24b7
mperron06 <m.perron06@gmail.com>
2015-01-26 17:08:59 +0100

    log to new or join private room

--- js/size.js
+++ js/size.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
  
       var width = $(window).width();
       var height = $(window).height();
+      var height_percent = height/100;
       
-      var height_col = height-height/100*4;
+      var height_col = height-height_percent*4;
       var height_dir = height_col/6;
       var height_conv = height_col*(1/2);
       var height_box = height_col/4;
+      var height_profil = height_col*(3/5);
       var height_map = document.getElementById("localVideo").offsetHeight;
         /*$('body').prepend('<div>' +  width + " px de        large sur " + height + ' px de haut </div>');*/
         
@@ -20,6 +22,7 @@ $(document).ready(function(){
       document.getElementById("dataChannelReceive").style.height= height_conv+"px";
       document.getElementById("map_canvas").style.height= height_box+"px";
       document.getElementById("historical-container-area").style.height= height_box+"px";
+      document.getElementById("profils-container").style.height= height_profil+"px";
         
       document.getElementById("map_canvas").style.height= height_map+"px";
       
