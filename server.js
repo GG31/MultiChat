76a7782157480bbf0e3a669bb22a5c4a3bd0e8e2
Genevieve Cirera <genevieve.cirera@gmail.com>
2015-02-05 16:00:15 +0100

    access control

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.all('/*', function(req, res, next) {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.setHeader("Access-Control-Allow-Origin", "*");
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
      next();
    });
 });
