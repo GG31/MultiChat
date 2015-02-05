eacd9034923c75367b45b48fc0fccc0c3d2b294d
Benjamin Vella <bvella@polytech.unice.fr>
2015-02-05 11:57:38 +0100

    move enabling of chat in joined instead of join

--- js/socketOn.js
+++ js/socketOn.js
@@ -15,7 +15,6 @@ function setOnMethods(socket){
       console.log('Another peer made a request to join room ' + room);
       console.log('This peer is the initiator of room ' + room + '!');
       setIsChannelReady(true);
-      enableMessageInterface(true);
     });
 
     // Si on reçoit le message "joined" alors on a rejoint une salle existante
@@ -25,6 +24,7 @@ function setOnMethods(socket){
       console.log('This peer has joined room ' + room);
       roomJoinedLog(room);
       getFullHistory();
+      enableMessageInterface(true);
       setIsChannelReady(true);
     });
 
