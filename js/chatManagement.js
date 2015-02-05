b9f9f933f0c9956f5e1b9e5c747bc83baf4793b9
Benjamin Vella <bvella@polytech.unice.fr>
2015-02-05 09:03:49 +0100

    Chat corrections

--- js/chatManagement.js
+++ js/chatManagement.js
@@ -2,11 +2,12 @@ var classes = ["color1","color2","color3","color4"];
 var classIndex = 0;
 
 function sendNewChat(){
+    $("#dataChannelSend").attr("value","");
     getSocket().emit('newMessage',$('#dataChannelSend').val());
 }
 
 function appendNewChat(user,newMessage){
-    $('#chat-table').append("<div class='"+classes[classIndex]+"'>"+user+"&nbsp;:</div>"+"<div class='"+classes[classIndex]+"-paragraph'>"+newMessage+"</div>");
+    $('#dataChannelReceive').append("<div class='"+classes[classIndex]+"'>"+user+"&nbsp;:</div>"+"<div class='"+classes[classIndex]+"-paragraph'>"+newMessage+"</div>");
     classIndex = (classIndex + 1)%classes.length;
 }
 
