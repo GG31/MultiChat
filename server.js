16f80cf7d2c19b3793d49ec93d9e01dfe7435216
Genevieve Cirera <genevieve.cirera@gmail.com>
2015-02-05 08:47:37 +0100

    files

--- server.js
+++ server.js
@@ -74,6 +74,7 @@ io.sockets.on('connection', function (socket){
 	
 	socket.on('newMessage', function(text){
 	   // echo to room 1 the message of username
+	   console.log ("NEW MESSAGE !!! " + text);
 		io.sockets.in(socket.room).emit('updatechat', socket.username, text);
 	   var date = new Date(Date.now());
       insertMessage(socket.username, socket.room, date, text);
